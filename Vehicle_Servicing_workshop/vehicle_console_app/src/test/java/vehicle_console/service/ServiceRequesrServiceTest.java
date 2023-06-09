21c0f07714c1ecf27238d84d807d632fd89305aa
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-09 10:53:52 +0530

    created test case for serviceRequestService

---
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/ServiceRequesrServiceTest.java
@@ -0,0 +1,16 @@
+package vehicle_console.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import opeartions.service.ServiceRequestService;
+
+class ServiceRequesrServiceTest {
+
+	@Test
+	void testServiceRequestServce() {
+		ServiceRequestService.getAllServiceRequests();
+	}
+
+}
