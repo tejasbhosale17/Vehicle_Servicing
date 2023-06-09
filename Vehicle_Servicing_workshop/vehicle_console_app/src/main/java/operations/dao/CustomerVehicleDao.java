d6f5473f27215a7ae7b40276362169fd165b39d2
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-09 10:22:42 +0530

    created functionality searchThisVehicle

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/CustomerVehicleDao.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/CustomerVehicleDao.java
@@ -15,6 +15,8 @@ static Connection con;
 	public CustomerVehicleDao() throws SQLException{
 		this.con=DBUtil.getConnection();
 	}
+
+	//---------------------------------------------------------------------
 	
 	
 	public void getAllVehicles(List<Customer_vehicles> CVList) {
@@ -32,7 +34,25 @@ static Connection con;
 		}
 		
 	}
-
+//--------------------------------------------------------------------------------------
+	public Customer_vehicles searchThisVehicle(String vnumber) {
+		String q6="select * from customer_vehicles where  vehicle_number = ?";
+		try {
+			PreparedStatement pmt= con.prepareStatement(q6);
+			pmt.setString(1, vnumber);
+			ResultSet rs=pmt.executeQuery();
+			if(rs.next()) {
+				System.out.println("Customer's Vehicle Exists");
+				Customer_vehicles cv =new Customer_vehicles(rs.getString("vehicle_number"),rs.getInt("customer_id"),rs.getInt("vehicle_id"));
+				return cv;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 	
 
 }
