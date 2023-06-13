345d268bb090132c5c724fc65b1c875f7bc7027d
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-13 15:38:56 +0530

    created test case for testDetailsofAllCustomerVehicles()

--- Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/CustomerVehicleServiceTest.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/CustomerVehicleServiceTest.java
@@ -2,19 +2,28 @@ package vehicle_console.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import opeartions.service.CustomerVehicleService;
+import operations.models.Customer_Vehicle_Details;
 
 class CustomerVehicleServiceTest {
 
-	@Test
-	void testGetAllCustomers() {
-		CustomerVehicleService.getAllCustVehicles();
-	}
+//	@Test
+//	void testGetAllCustomers() {
+//		CustomerVehicleService.getAllCustVehicles();
+//	}
+//	
+//	@Test
+//	void testFindThatCustomerVehicle(){
+//		CustomerVehicleService.findThisVehicle();
+//	}
 	
 	@Test
-	void testFindThatCustomerVehicle(){
-		CustomerVehicleService.findThisVehicle();
+	void testDetailsofAllCustomerVehicles() {
+		int customer_id=2;
+		CustomerVehicleService.DetailsofAllCustomerVehicles(customer_id);
 	}
-
 }
