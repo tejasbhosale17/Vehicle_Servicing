f33e3da9d7f39779ca44323a1a20959d33e883fa
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-13 12:19:21 +0530

    created test case for VehicleMenu

---
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/VehicleMenuTest.java
@@ -0,0 +1,16 @@
+package vehicle_console.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import opeartions.Menu.VehicleMenu;
+
+class VehicleMenuTest {
+
+	@Test
+	void testVehicleMenu() {
+		VehicleMenu.ChooseVehicleMain();
+	}
+
+}
