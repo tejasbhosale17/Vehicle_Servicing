ae94d50eaba887bae5e7dac4cf1948eb6595ce41
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-14 14:56:07 +0530

    created test case for VehicleService

--- Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/VehicleServiceTest.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/VehicleServiceTest.java
@@ -9,8 +9,38 @@ import opeartions.service.VehicleService;
 class VehicleServiceTest {
 
 	@Test
+	void testgetAllVehicles() {
+		VehicleService.getAllVehicles();
+	}
+
+	
+	@Test
+	void testgetSpecificVehicle() {
+		VehicleService.getSpecificVehicle();
+	}
+	
+	@Test
+	void testremoveVehiclebyId() {
+		VehicleService.removeVehiclebyId();
+	}
+	
+	@Test
+	void testremoveVehiclebyVNumber() {
+		VehicleService.removeVehiclebyVNumber(" MH01TY8814");
+	}
+	
+	@Test
+	void testupdateVehicle() {
+		VehicleService.updateVehicle();
+	}
+	
+	@Test
+	void testaddVehicle() {
+		VehicleService.addVehicle();
+	}
+	
+	@Test
 	void testaddVehicleUsingMobile() {
 		VehicleService.addVehicleUsingMobile();
 	}
-
 }
