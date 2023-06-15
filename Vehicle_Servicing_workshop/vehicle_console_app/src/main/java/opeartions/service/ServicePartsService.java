6649c866f27302ca423cb8dd8715e1eb5b1a3223
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-15 12:13:19 +0530

    created getAllServiceParts function

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/ServicePartsService.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/ServicePartsService.java
@@ -1,14 +1,28 @@
 package opeartions.service;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import operations.dao.ServicePartsDao;
+import operations.models.Service_Parts;
 
 public class ServicePartsService {
 
 	
 	static Scanner scan = new Scanner(System.in);
 	
-	public void getAllServiceParts() {
-		System.out.println("Enter Parts_id");
-		int Service_id=scan.nextInt();
+	public static void getServiceParts() {
+		List<Service_Parts> SPList = new ArrayList<>();
+		try {
+			ServicePartsDao servicePartsDao = new ServicePartsDao();
+			SPList=servicePartsDao.getAllServiceParts(SPList);
+			for(Service_Parts sp : SPList) {
+				System.out.println(sp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/ServicePartsDao.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/ServicePartsDao.java
@@ -1,5 +1,34 @@
 package operations.dao;
 
-public class ServicePartsDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import dbUtil.DBUtil;
+import operations.models.Service_Parts;
+
+public class ServicePartsDao {
+	private static Connection con;
+
+	public ServicePartsDao() throws SQLException {
+		this.con = DBUtil.getConnection();
+	}
+
+	public List<Service_Parts> getAllServiceParts(List<Service_Parts> SPList) {
+		String q1="select * from service_parts";
+		try {
+			PreparedStatement pmt = con.prepareStatement(q1);
+			ResultSet rs =pmt.executeQuery();
+			while(rs.next()) {
+				Service_Parts sp =new Service_Parts(rs.getInt("service_id"),rs.getInt("part_id"),rs.getInt("quantity"));
+				SPList.add(sp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return SPList;
+	}
 }
