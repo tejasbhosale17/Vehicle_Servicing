2a7b56f18874a0f835c215e6fd7b5f7ed6ec2a11
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-15 12:15:39 +0530

    created test case for ServicePartsService and tested GetAllServiceParts Method

---
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/ServicePartsServiceTest.java
@@ -0,0 +1,16 @@
+package vehicle_console.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import opeartions.service.ServicePartsService;
+
+class ServicePartsServiceTest {
+
+	@Test
+	void testgetServiceParts() {
+		ServicePartsService.getServiceParts();
+	}
+
+}
