032b4e85846cd78c6599891bcc50960d46da54f4
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-15 14:52:28 +0530

    created and tested test case for partsDao

---
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console_dao/PartDaoTest.java
@@ -0,0 +1,100 @@
+package vehicle_console_dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import operations.dao.PartDao;
+import operations.models.Part;
+
+class PartDaoTest {
+
+	@Test
+	void testgetAppParts() {
+		List<Part> partList = new ArrayList<>();
+		try {
+			PartDao partDao = new PartDao();
+			partDao.getAppParts(partList);
+			for(Part p : partList) {
+				System.out.println(p);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void testfindThisPartById() {
+		try {
+			PartDao partDao = new PartDao();
+			Part part = new Part();
+			part=partDao.findThisPartById(5);
+			if(part!=null) {
+				System.out.println(part);
+			}else {
+				System.out.println("Part not found.. :(");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	@Test
+	void testremoveThisPart() {
+		try {
+			PartDao partDao = new PartDao();
+			int isRemoved=partDao.removeThisPart(8);
+			if(isRemoved>0) {
+				System.out.println("Part Removed Succesfully!");
+			}else {
+				System.out.println("Part Not Found...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void testupdateThisPart() {
+		System.out.println("Enter part id and price to update");
+		try {
+			PartDao partDao = new PartDao();
+			int isUpdated=partDao.updateThisPart(8,1200);
+			if(isUpdated>0) {
+				System.out.println(isUpdated+" Part Updated Sucesfully!");
+			}else {
+				System.out.println("Part not found...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void testaddThisPart() {
+		try {
+
+			PartDao partDao = new PartDao();
+			int isInserted =partDao.addThisPart(8,"horn","soundsLOUD",800);
+			if(isInserted>0) {
+				System.out.println(isInserted+" Part Added Succesfully!");
+			}else{
+				System.out.println("Part not Found...");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	
+	
+}
