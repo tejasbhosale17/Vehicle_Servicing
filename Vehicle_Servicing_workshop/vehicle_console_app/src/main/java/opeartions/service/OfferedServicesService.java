0fa2be42de3e6ddf9555c4d3e659029a0eb72678
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-20 15:29:34 +0530

    minor changes

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/OfferedServicesService.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/OfferedServicesService.java
@@ -13,38 +13,38 @@ public class OfferedServicesService {
 
 	static Scanner scan = new Scanner(System.in);
 	
-	public static void getAllServices(){
-		List<Service> serviceList = new ArrayList<>();
-		try {
-			ServiceDao serviceDao = new ServiceDao();
-			serviceDao.getAllservices(serviceList);
-			for(Service s: serviceList) 
-			{
-				System.out.println("1"+s);
-			}
-		} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		}
-	}
-//------------------------------------------------------------------------------------------
-	public static void thisServiceThere() {
-		try {
-			System.out.println("Enter Service_id:");
-			int sid=scan.nextInt();
-			ServiceDao serviceDao = new ServiceDao();
-			Service s=serviceDao.findThisService(sid);
-			if(s!=null) {
-				System.out.println("Service Already Exists!!");
-				System.out.println(s);
-			}else {
-				System.out.println("Service Does not Exists!!");
-			}
-		} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		}	
-	}
+//	public static void getAllServices(){
+//		List<Service> serviceList = new ArrayList<>();
+//		try {
+//			ServiceDao serviceDao = new ServiceDao();
+//			serviceDao.getAllservices(serviceList);
+//			for(Service s: serviceList) 
+//			{
+//				System.out.println("1"+s);
+//			}
+//		} catch (SQLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//		}
+//	}
+////------------------------------------------------------------------------------------------
+//	public static void thisServiceThere() {
+//		try {
+//			System.out.println("Enter Service_id:");
+//			int sid=scan.nextInt();
+//			ServiceDao serviceDao = new ServiceDao();
+//			Service s=serviceDao.findThisService(sid);
+//			if(s!=null) {
+//				System.out.println("Service Already Exists!!");
+//				System.out.println(s);
+//			}else {
+//				System.out.println("Service Does not Exists!!");
+//			}
+//		} catch (SQLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//		}	
+//	}
 //------------------------------------------------------------------------------------------
 	public static void RemoveThisService() {
 		int sid=scan.nextInt();
