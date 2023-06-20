0a226e08ab0c9557f05cf20b45c5fb1eea58ac6e
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-20 15:28:25 +0530

    created Oil model and extended Service on it

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Oil.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Oil.java
@@ -1,6 +1,38 @@
 package operations.models;
 
-public class Oil {
-
+public class Oil extends Service{
 	double oil_cost;
+
+
+	public Oil() {
+		super("oil");
+		// TODO Auto-generated constructor stub
+	}
+
+	public Oil(double oil_cost) {
+		super();
+		this.oil_cost = oil_cost;
+	}
+	
+
+	public double getOil_cost() {
+		return oil_cost;
+	}
+
+	public void setOil_cost(double oil_cost) {
+		this.oil_cost = oil_cost;
+	}
+
+	@Override
+	public void acceptService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calculateTotalCost() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
