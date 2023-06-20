d59a2502adf4c3c302e0a108b346a6c2ce0eba9b
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-20 15:58:27 +0530

    made changes in oil and maintainance created constrctor

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Maintainance.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Maintainance.java
@@ -14,6 +14,36 @@ public class Maintainance extends Service{
 		this.labour_charges = 0;
 		this.partList = new ArrayList<Service_Parts>();
 	}
+	
+	
+	
+	
+	
+
+	public Maintainance(int service_id, String type,double labour_charges, double total_cost, String remark, int service_request_id) {
+		super(service_id, type, total_cost, remark, service_request_id);
+		this.service_id = service_id;
+		this.type = type;
+		this.labour_charges=labour_charges;
+		this.total_cost = total_cost;
+		this.remark = remark;
+		this.service_request_id = service_request_id;
+	}
+
+
+
+
+
+
+	public Maintainance(String type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
 
 	public double getLabour_charges() {
 		return labour_charges;
--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Oil.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/models/Oil.java
@@ -15,6 +15,22 @@ public class Oil extends Service{
 	}
 	
 
+	public Oil(int service_id, String type,double oil_cost, double total_cost, String remark, int service_request_id) {
+		super(service_id, type, total_cost, remark, service_request_id);
+		this.service_id = service_id;
+		this.type = type;
+		this.oil_cost = oil_cost;
+		this.total_cost = total_cost;
+		this.remark = remark;
+		this.service_request_id = service_request_id;
+		
+	}
+
+	public Oil(String type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
 	public double getOil_cost() {
 		return oil_cost;
 	}
