ff2ad24542084abbfc837eca99424fd553590d32
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-21 11:38:29 +0530

    created new test called testFindThatService

--- Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/OfferedServicesServiceTest.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console/service/OfferedServicesServiceTest.java
@@ -2,21 +2,31 @@ package vehicle_console.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import opeartions.service.OfferedServicesService;
+import operations.models.Service;
 
 class OfferedServicesServiceTest {
 //
-	@Test
-	void testGetAllServices() {
-		OfferedServicesService.getAllServices();
-	}
-//
 //	@Test
-//	void testFindThatService() {
-//		OfferedServicesService.thisServiceThere();;
+//	void testGetAllServices() {
+//		OfferedServicesService.getAllServices();
 //	}
+//
+	@Test
+	void testFindThatService() {
+		List<Service> serviceList = new ArrayList<>();
+		int service_request_id=23;
+		serviceList=OfferedServicesService.thisServiceThere(serviceList,service_request_id);;
+		for(Service s: serviceList) 
+		{
+			System.out.println(s);
+		}
+	}
 //	
 //	@Test
 //	void testaddTheService() {
