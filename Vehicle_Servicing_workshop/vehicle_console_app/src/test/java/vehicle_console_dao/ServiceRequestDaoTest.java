3c9b40ac39475c05192052519c89f40e07ff889c
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-21 11:33:31 +0530

    created test case for serviceRequestDao

---
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/test/java/vehicle_console_dao/ServiceRequestDaoTest.java
@@ -0,0 +1,14 @@
+package vehicle_console_dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServiceRequestDaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
