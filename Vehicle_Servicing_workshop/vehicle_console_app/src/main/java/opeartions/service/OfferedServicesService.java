1faa61157ce722b05ba21c84afb62842ef626492
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-22 11:40:39 +0530

    created function to add service,if service exists then update service

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/OfferedServicesService.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/OfferedServicesService.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Scanner;
 
 import operations.dao.ServiceDao;
-
+import operations.models.Maintainance;
+import operations.models.Oil;
 import operations.models.Service;
+import operations.models.Service_requests;
 
 public class OfferedServicesService {
 
@@ -27,7 +29,7 @@ public class OfferedServicesService {
 		e.printStackTrace();
 		}
 	}
-////------------------------------------------------------------------------------------------
+////---------------------------------------------------------------------------------------------------
 	public static List<Service> thisServiceThere(List<Service> serviceList,int service_request_id) {
 //		List<Service> serviceList = new ArrayList<>();
 		try {
@@ -45,7 +47,7 @@ public class OfferedServicesService {
 		}
 		return serviceList;	
 	}
-//------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
 	public static void RemoveThisService() {
 		int sid=scan.nextInt();
 		int isRemoved=0;
@@ -65,76 +67,124 @@ public class OfferedServicesService {
 
 //-------------------------------------------------------------------------------------
 	
-	public static void updateThisService() {
-		System.out.println("enter service_id of the service want to update:");
-		int sid=scan.nextInt();
-		System.out.println("enter labour_charges and total_charges want to change:");
-		double lc=scan.nextInt();
-		double tc=scan.nextInt();
-		int isUpdated=0;
-		try {
-			ServiceDao serviceDao  =new ServiceDao();
-			isUpdated=serviceDao.updateThisService(sid,lc,tc);
-			if(isUpdated > 0) {
-				System.out.println("Service Exists and Updated!!");
-			}else {
-				System.out.println("Service Does Not Exists!!");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+//	public static void updateThisService(Service s) {
+//		System.out.println("enter service_id of the service want to update:");
+//		int sid=s.getService_id()
+//		System.out.println("enter labour_charges and total_charges want to change:");
+//		double lc=
+//		double tc=scan.nextInt();
+//		int isUpdated=0;
+//		try {
+//			ServiceDao serviceDao  =new ServiceDao();
+//			isUpdated=serviceDao.updateThisService(sid,lc,tc);
+//			if(isUpdated > 0) {
+//				System.out.println("Service Exists and Updated!!");
+//			}else {
+//				System.out.println("Service Does Not Exists!!");
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
 //-------------------------------------------------------------------------------------
-	public static void addTheService() {
-		System.out.println("Enter service_id,type,oil_charges,labour charges,total_amount,remark,service_request_id");
-		int isAdded=0;
-		int service_id=scan.nextInt();
-		System.out.println("choose type: oil/maintainance");
-		String type=scan.next();
-		double oil_cost=scan.nextDouble();
-		double labour_charges=scan.nextDouble();
-		double total_cost=scan.nextDouble();
-		System.out.println("Enter remark");
-		scan.nextLine();
-		String remark=scan.nextLine();
-		System.out.println("Enter vehicle number");
-		String vehicle_number=scan.next();
-		ServiceRequestService.getServiceRequestByVehicleNumber(vehicle_number);
-		System.out.println("Enter Service request id");
-		int service_request_id=scan.nextInt();
-		double bill_amount=oil_cost+labour_charges;
-		
-//		ServiceRequestService.addServiceRequest(service_request_id, vehicle_number, bill_amount);
-		try {
-			ServiceDao serviceDao  =new ServiceDao();
-			isAdded=serviceDao.addThisService(service_id, type, oil_cost, labour_charges, total_cost, remark, service_request_id);
-			if(isAdded>0) {
-				System.out.println("Service Inserted Succesfully...");
-			}else {
-				System.out.println("Insertion of Service Unsuccesfull...");
-			}
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
-		
-	}
+//	public static void addTheService() {
+//		System.out.println("Enter service_id,type,oil_charges,labour charges,total_amount,remark,service_request_id");
+//		int isAdded=0;
+//		int service_id=scan.nextInt();
+//		System.out.println("choose type: oil/maintainance");
+//		String type=scan.next();
+//		double oil_cost=scan.nextDouble();
+//		double labour_charges=scan.nextDouble();
+//		double total_cost=scan.nextDouble();
+//		System.out.println("Enter remark");
+//		scan.nextLine();
+//		String remark=scan.nextLine();
+//		System.out.println("Enter vehicle number");
+//		String vehicle_number=scan.next();
+//		ServiceRequestService.getServiceRequestByVehicleNumber(vehicle_number);
+//		System.out.println("Enter Service request id");
+//		int service_request_id=scan.nextInt();
+//		double bill_amount=oil_cost+labour_charges;
+//		
+////		ServiceRequestService.addServiceRequest(service_request_id, vehicle_number, bill_amount);
+//		try {
+//			ServiceDao serviceDao  =new ServiceDao();
+//			isAdded=serviceDao.addThisService(service_id, type, oil_cost, labour_charges, total_cost, remark, service_request_id);
+//			if(isAdded>0) {
+//				System.out.println("Service Inserted Succesfully...");
+//			}else {
+//				System.out.println("Insertion of Service Unsuccesfull...");
+//			}
+//		} catch (SQLException e) {
+//			
+//			e.printStackTrace();
+//		}
+//		
+//	}
 //------------------------------------------------------------------------------
 	
-	public static void addingThisService() {
-		System.out.println("Enter service_id,type,oil_charges,labour charges,total_amount,remark,service_request_id");
-		int isAdded=0;
-		try {
-			ServiceDao serviceDao  =new ServiceDao();
-			System.out.println("Enter remark first");
+//	public static void addingThisService() {
+//		System.out.println("Enter service_id,type,oil_charges,labour charges,total_amount,remark");
+//		int isAdded=0;
+//		try {
+//			ServiceDao serviceDao  =new ServiceDao();
+//			System.out.println("Enter remark first");
+//			String remark=scan.nextLine();
+//			serviceDao.addThisService(scan.nextInt(), scan.next(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), remark, scan.nextInt());
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public static void addThisServiceByMaintainance(Service_requests sr) {
+		List<Service> srList = new ArrayList<>();
+		srList=sr.getServiceList();
+		if(isEmpty(srList)) {
+			System.out.println("Enter Service_id,type,labour_charges,total_cost,remark,service_request_id");
+			int sid=scan.nextInt();
+			String type=scan.next();
+			double labour_charges=scan.nextDouble();
+			double total_cost=scan.nextDouble();
+			scan.nextLine();
 			String remark=scan.nextLine();
-			serviceDao.addThisService(scan.nextInt(), scan.next(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), remark, scan.nextInt());
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			int srid=sr.getService_request_id();
+			Maintainance m = new Maintainance(sid,type,labour_charges,total_cost,remark,srid);
+			ServiceDao.addThisServiceByMaintainance(m);
+			
+		}else {
+			for(Service s:srList) {
+				if(s instanceof  Maintainance) {
+					System.out.println("Enter Labour_charges:");
+					double labour_charges=scan.nextDouble();
+					double oil_cost=0;
+					int sid=s.getService_id();
+					
+					ServiceDao.updateThisService(sid,oil_cost,labour_charges);
+				}else if(s instanceof Oil) {
+					System.out.println("Enter Oil_cost:");
+					double oil_cost=scan.nextDouble();
+					double labour_charges=0;
+					int sid=s.getService_id();
+					ServiceDao.updateThisService(sid,oil_cost,labour_charges);
+				}
+			}
+			
 		}
+		
+	}
+	
+	
+//-------------To check if The ServiceList is Empty--------------
+	public static boolean isEmpty(List<Service> srList) {
+//		List<Service> srList = new ArrayList<>();
+//		srList=srList2.getServiceList();
+		if(srList==null) {
+			return true;
+		}
+		return false;
 	}
 	
 }
