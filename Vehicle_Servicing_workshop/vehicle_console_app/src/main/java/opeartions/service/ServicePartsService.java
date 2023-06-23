2264845003d0ac72707c536010009aece748045f
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-23 15:30:26 +0530

    created service and dao for getAllServiceParts

--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/ServicePartsService.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/opeartions/service/ServicePartsService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 import operations.dao.ServicePartsDao;
+import operations.models.Part;
+import operations.models.Service;
 import operations.models.Service_Parts;
 
 public class ServicePartsService {
@@ -13,16 +15,38 @@ public class ServicePartsService {
 	
 	static Scanner scan = new Scanner(System.in);
 	
-//	public static void getServiceParts() {
-//		List<Service_Parts> SPList = new ArrayList<>();
-//		try {
-//			ServicePartsDao servicePartsDao = new ServicePartsDao();
-//			SPList=servicePartsDao.getAllServiceParts(SPList);
-//			for(Service_Parts sp : SPList) {
-//				System.out.println(sp);
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void getServiceParts() {
+		List<Service_Parts> SPList = new ArrayList<>();
+		try {
+			ServicePartsDao servicePartsDao = new ServicePartsDao();
+			SPList=servicePartsDao.getAllServiceParts(SPList);
+			for(Service_Parts sp : SPList) {
+				System.out.println(sp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static Service_Parts addServiceParts(Service_Parts sp) {
+		int isAdded=0;
+		
+		try {
+			
+			ServicePartsDao servicePartsDao = new ServicePartsDao();
+			isAdded=servicePartsDao.addThisServicePart(sp);
+			if(isAdded>0) {
+				System.out.println("Inserted ServiceParts...");
+			}else {
+				System.out.println("Can not Insert ServicePart...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sp;
+	}
+	
+	
 }
--- Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/ServicePartsDao.java
+++ Vehicle_Servicing_workshop/vehicle_console_app/src/main/java/operations/dao/ServicePartsDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 import dbUtil.DBUtil;
+import operations.models.Part;
+import operations.models.Service;
 import operations.models.Service_Parts;
 
 public class ServicePartsDao {
@@ -16,19 +18,41 @@ public class ServicePartsDao {
 		this.con = DBUtil.getConnection();
 	}
 
-//	public List<Service_Parts> getAllServiceParts(List<Service_Parts> SPList) {
-//		String q1="select * from service_parts";
-//		try {
-//			PreparedStatement pmt = con.prepareStatement(q1);
-//			ResultSet rs =pmt.executeQuery();
-//			while(rs.next()) {
-//				Service_Parts sp =new Service_Parts(rs.getInt("service_id"),rs.getInt("part_id"),rs.getInt("quantity"));
-//				SPList.add(sp);
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return SPList;
+//	public List<Service_Parts> getAllServiceParts(List<Service_Parts> sPList) {
+//		// TODO Auto-generated method stub
+//		return null;
 //	}
+
+	public List<Service_Parts> getAllServiceParts(List<Service_Parts> SPList) {
+		String q1="select * from service_parts";
+		try {
+			PreparedStatement pmt = con.prepareStatement(q1);
+			ResultSet rs =pmt.executeQuery();
+			while(rs.next()) {
+				Service_Parts sp =new Service_Parts(rs.getInt("service_id"),rs.getInt("part_id"),rs.getInt("quantity"));
+				SPList.add(sp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return SPList;
+	}
+
+	public int addThisServicePart(Service_Parts sp) {
+		String q2="insert into service_parts values(?,?,?)";
+		int nspa=0;
+		try {
+			PreparedStatement pmt= con.prepareStatement(q2);
+			pmt.setInt(1, sp.getService_id());
+			pmt.setInt(2, sp.getPart_id());
+			pmt.setInt(3, sp.getQuantity());
+			nspa=pmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return nspa;
+		
+	}
 }
